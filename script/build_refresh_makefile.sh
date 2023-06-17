443cb616ee720e2c5002e1c065e8da041ba515c2
Juyeong Maing <mjy9088@naver.com>
2023-06-17 19:48:11 +0100

    add info message on generate Makefile

--- script/build_refresh_makefile.sh
+++ script/build_refresh_makefile.sh
@@ -4,11 +4,18 @@ set -e
 
 cd "$(dirname "$0")"
 
+generate_makefile() {
+  printf "\033[0;33m[INFO]\033[0m generating Makefile..."
+  sh build_makefile.sh > ../build/Makefile
+}
+
 if [ ! -f ../build/files.txt ]; then
 
-  find ../include ../src -name "*.c" -o -name "*.o" | sort > ../build/files.txt
+  find ../include ../src -name "*.c" -o -name "*.o" | sort > ../build/files.new.txt
 
-  sh build_makefile.sh > ../build/Makefile
+  generate_makefile
+
+  mv ../build/files.new.txt ../build/files.txt
 
 else
 
@@ -18,13 +25,11 @@ else
 
   trap cleanup EXIT
 
-  cleanup
-
-  mv ../build/files.txt ../build/files.old.txt
+  mv -f ../build/files.txt ../build/files.old.txt
 
   find ../include ../src -name "*.c" -o -name "*.o" | sort > ../build/files.new.txt
 
-  cmp -s ../build/files.old.txt ../build/files.new.txt || sh build_makefile.sh > ../build/Makefile
+  cmp -s ../build/files.old.txt ../build/files.new.txt || generate_makefile
 
   mv ../build/files.new.txt ../build/files.txt
 
