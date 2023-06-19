a4a9d155585fe3f971f3067b72ea465f06e02297
맹주영 <mjy9088@naver.com>
2023-06-19 14:53:32 +0900

    fix dirty git working tree

--- common.mk
+++ common.mk
@@ -1,10 +1,21 @@
-include targets.mk
+-include targets.mk
+
+TARGET = \
+	out/exe/minirt.0.exe \
+	out/exe/minirt.1.exe \
+	out/exe/minirt.2.exe \
 
 all: $(TARGET)
 .PHONY: all
 
 .PHONY: everything
-everything: $(EVERYTHING) .vscode/launch.json .vscode/tasks.json
+everything: error_if_targets_not_included $(EVERYTHING) .vscode/launch.json .vscode/tasks.json
+
+.PHONY: error_if_targets_not_included
+error_if_targets_not_included:
+	@$(FLAG_IF_TARGETS_INCLUDED)([ -t ] && printf "\033[0;31m[ERROR]\033[0m" || printf "[ERROR]")
+	@$(FLAG_IF_TARGETS_INCLUDED)echo " targets.mk is not generated. run \`sh metamake.sh\` once."
+	@$(FLAG_IF_TARGETS_INCLUDED)false
 
 .PHONY: clean
 clean:
