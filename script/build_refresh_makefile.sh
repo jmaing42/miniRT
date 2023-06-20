894d28a88ead1cfd77abd6ba3207570735309a44
Juyeong Maing <mjy9088@naver.com>
2023-06-20 18:13:35 +0900

    add message for Makefile generation end

--- script/build_refresh_makefile.sh
+++ script/build_refresh_makefile.sh
@@ -5,9 +5,10 @@ set -e
 cd "$(dirname "$0")"
 
 generate_makefile() {
-  [ ! -t 1 ] || printf "\033[0;33m[INFO]\033[0m generating Makefile...\n"
-  [ -t 1 ] || printf "[INFO] generating Makefile...\n"
+  [ ! -t 1 ] || printf "\033[0;33m[INFO]\033[0m generating Makefile..."
+  [ -t 1 ] || printf "[INFO] generating Makefile..."
   sh build_makefile.sh > ../build/Makefile
+  printf " done!\n"
 }
 
 if [ ! -f ../build/files.txt ]; then
