7596dfaaf8f946f82947a36c0dcba4687833ac8a
맹주영 <mjy9088@naver.com>
2023-06-26 21:05:16 +0900

    fix bug

--- test/mocks/branch/mock_branch_include.mk
+++ test/mocks/branch/mock_branch_include.mk
@@ -2,9 +2,10 @@ MOCK_BRANCH_UNAME_S := $(shell uname -s)
 ifeq ($(MOCK_BRANCH_UNAME_S),Linux)
 MOCK_BRANCH_LDFLAGS := -Wl,--wrap=malloc -Wl,--wrap=free -Wl,--wrap=open -Wl,--wrap=close -Wl,--wrap=read -Wl,--wrap=write
 else
-ifeq (($(MOCK_BRANCH_UNAME_S)),Darwin)
+ifeq ($(MOCK_BRANCH_UNAME_S),Darwin)
 MOCK_BRANCH_CPPFLAGS := -DMOCK_BRANCH_NO_WRAP
 MOCK_BRANCH_LDFLAGS := -ldl
+else
+$(error Unrecognized OS: $(MOCK_BRANCH_UNAME_S))
 endif
-$(error Unrecognized OS)
 endif
