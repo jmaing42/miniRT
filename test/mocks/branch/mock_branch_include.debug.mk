cccb3701b06620877352c7459cefac0984868755
맹주영 <mjy9088@naver.com>
2023-06-26 21:40:59 +0900

    fix bug on macos

--- test/mocks/branch/mock_branch_include.debug.mk
+++ test/mocks/branch/mock_branch_include.debug.mk
@@ -3,7 +3,7 @@ ifeq ($(MOCK_BRANCH_UNAME_S),Linux)
 MOCK_BRANCH_LDFLAGS := -Wl,--wrap=malloc -Wl,--wrap=free -Wl,--wrap=open -Wl,--wrap=close -Wl,--wrap=read -Wl,--wrap=write
 else
 ifeq ($(MOCK_BRANCH_UNAME_S),Darwin)
-MOCK_BRANCH_LDLIBS := -ldl $(BASE_PATH)/test/mocks/branch/out/libmock_branch.a
+MOCK_BRANCH_LDLIBS := -ldl $(BASE_PATH)/test/mocks/branch/out/libmock_branch.debug.a
 else
 $(error Unrecognized OS: $(MOCK_BRANCH_UNAME_S))
 endif
